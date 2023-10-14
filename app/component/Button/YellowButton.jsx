36c02b05eb990cbbaa270a123b081611124b8f87
Prince Asiedu <princekofasiedu@gmail.com>
2023-10-14 16:08:22 +0000

    Created YellowButton component

--- app/component/Button/YellowButton.jsx
+++ app/component/Button/YellowButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const YellowButton = () => {
+const YellowButton = ({content}) => {
   return (
-    <button className=''></button>
-  )
+    <button className="bg-[#e5b970] text-[#2d2d2d] p-[15px] h-[50px] rounded-full w-[170px] text-sm mr-[15px] transition-all hover:bg-[#ffbe56]">
+      {content}
+    </button>
+  );
 }
 
 export default YellowButton
