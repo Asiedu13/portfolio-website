891c427ebc59d3a3bc69ae520de138dbafb557d9
Prince Asiedu <princekofasiedu@gmail.com>
2023-10-21 16:08:48 +0000

    Edited nav design

--- app/component/Nav/Nav.jsx
+++ app/component/Nav/Nav.jsx
@@ -12,9 +12,11 @@ const Nav = () => {
         </p>
       </div>
       {/* Navigation itself */}
-      <div className="group/item w-[200px] h-[fit-content] right-[0px] bg-[#2d2d2d] flex flex-col absolute md:flex-row md:relative justify-between md:w-[900px] md:h-[80px] md:bg-[transparent] ">
-        <button className="group/item h-[60px] md:hidden">Menu</button>
-        <ul className="flex flex-col h-[000px] w-[200px] invisible group-hover/item:visible group-hover/item:h-[inherit] md:flex md:flex-row md:w-[600px] md:items-center md:justify-around md:h-[80px] md:visible">
+      <div className="group/item w-[200px] h-[fit-content] right-[10px] flex flex-col absolute md:flex-row md:relative justify-between md:w-[900px] md:h-[80px] md:bg-[transparent]">
+        <button className="group/item h-[60px] md:hidden bg-[#2d2d2d] rounded-xl ">
+          Menu
+        </button>
+        <ul className="flex flex-col h-[000px] w-[200px] invisible group-hover/item:visible group-hover/item:h-[inherit] md:flex md:flex-row md:w-[600px] md:items-center md:justify-around md:h-[80px] md:visible bg-[#2d2d2d]">
           <Link href="/">
             <li className="text-[white] hover:text-[#dadeda] h-[50px] flex justify-center md:h-[inherit]">
               About
@@ -37,7 +39,7 @@ const Nav = () => {
           </Link>
         </ul>
         {/* Download CV button */}
-        <div className="items-center px-[30px] h-[0px] flex justify-center invisible group-hover/item:visible group-hover/item:h-[inherit] py-[.2rem] md:visible md:h-[inherit] md:py-[0]">
+        <div className="items-center px-[30px] h-[0px] flex justify-center invisible group-hover/item:visible group-hover/item:h-[inherit] py-[.2rem] md:visible md:h-[inherit] md:py-[0] bg-[#2d2d2d] rounded-b-xl">
           <button className="bg-[#fc9701] rounded-md p-[10px] text-sm text-[#000]  transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-[#ffa216] duration-300 ">
             Download CV
           </button>
